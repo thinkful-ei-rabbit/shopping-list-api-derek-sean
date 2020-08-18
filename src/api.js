58628d5529f1ddfe459393993cad3a18629b844a
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-08-18 09:22:42 -0700

    complete step 4

--- src/api.js
+++ src/api.js
@@ -4,6 +4,18 @@ function getItems() {
   return fetch(`${BASE_URL}/items`)
 }
 
+function createItem(name) {
+  let newItem = JSON.stringify({name: name});
+  return fetch(`${BASE_URL}/items`, {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: newItem
+  });
+}
+
 export default {
   getItems,
+  createItem,
 };
