e5c8aec84ef6b39166798e17ceff19f2c858506a
Derek "8-Bit" Nellis <dnellis3456@gmail.com>
2020-08-18 08:37:07 -0700

    first commit

---
+++ src/api.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/ds'
+
+function getItems() {
+  return Promise.resolve('Successful response!')
+}
+
+export default {
+  getItems,
+};
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 
+import api from './api'
 import shoppingList from './shopping-list';
 
+
+
 const main = function () {
+  api.getItems()
+  .then(res => console.log(res));
+  console.log(api.BASE_URL);
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
